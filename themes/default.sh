e82d1d2d7129d3591aec01b91a73353336ca70b2
Shohei Nakayama <sho1218ruri@gmail.com>
2016-05-10 23:18:16 +0900

    [change] セグメントの色を変更

--- themes/default.sh
+++ themes/default.sh
@@ -27,13 +27,13 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"hostname 33 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
-		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
-		"vcs_others 245 0" \
+		# "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		# "wan_ip 24 255" \
+		# "vcs_branch 29 88" \
+		# "vcs_compare 60 255" \
+		# "vcs_staged 64 255" \
+		# "vcs_modified 9 255" \
+		# "vcs_others 245 0" \
 	)
 fi
 
@@ -41,18 +41,19 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
 		"pwd 89 211" \
-		"mailcount 9 255" \
-		"now_playing 234 37" \
+		# "mailcount 9 255" \
+		# "now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 237 167" \
-		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
-		"weather 37 255" \
+		# "load 237 167" \
+		# "tmux_mem_cpu_load 234 230" \
+    "wifi_ 64 230" \
+		"battery_ 166 230" \
+		# "weather 37 255" \
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date 37 230" \
+		"time 37 230 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+    "date_day 37 230 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
