2ade5abab1034a550552131809df858c793d4d0b
Shohei Nakayama <sho1218ruri@gmail.com>
2016-05-12 13:42:09 +0900

    [clean] リファクタリング

--- segments/wifi_.sh
+++ segments/wifi_.sh
@@ -4,17 +4,18 @@ WIFI_NORMAL=""
 WIFI_WEAK=""
 
 run_segment() {
-  wifi_signal=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep "\sagrCtlRSSI" | sed -e "s/[^:]*: //" | sed -e "s/-//")
-  wifi_ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I |grep "\sSSID"|sed -e "s/[^:]*: //")
+  wifi=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
+  signal=$(echo "$wifi" | grep "\sagrCtlRSSI" | sed -e "s/[^:]*: //" | sed -e "s/-//")
+  ssid=$(echo "$wifi" |grep "\sSSID"|sed -e "s/[^:]*: //")
 
-  if [ $wifi_signal -le 39 ]; then
-    echo "${WIFI_VERY_STRONG} ${wifi_ssid}"
-  elif [ $wifi_signal -gt 39 ]&&[ $wifi_signal -le 49 ]; then
-    echo "${WIFI_STRONG} ${wifi_ssid}"
-  elif [ $wifi_signal -gt 49 ]&&[ $wifi_signal -le 59 ]; then
-    echo "${WIFI_NORMAL} ${wifi_ssid}"
-  elif [ $wifi_signal -gt 59 ]&&[ $wifi_signal -le 69 ]; then
-    echo "${WIFI_WEAK} ${wifi_ssid}"
+  if [ $signal -le 39 ]; then
+    echo "${WIFI_VERY_STRONG} ${ssid}"
+  elif [ $signal -gt 39 ]&&[ $signal -le 49 ]; then
+    echo "${WIFI_STRONG} ${ssid}"
+  elif [ $signal -gt 49 ]&&[ $signal -le 59 ]; then
+    echo "${WIFI_NORMAL} ${ssid}"
+  elif [ $signal -gt 59 ]&&[ $signal -le 69 ]; then
+    echo "${WIFI_WEAK} ${ssid}"
   else 
     echo "${WIFI_WEAK}"
   fi
