88363f2cf8eed7adc02d7a3322be71ea9ccf4213
Shohei Nakayama <sho1218ruri@gmail.com>
2016-05-13 16:33:26 +0900

    [change] Wi-Fiがなかった時の設定を追加

--- segments/wifi_.sh
+++ segments/wifi_.sh
@@ -8,15 +8,17 @@ run_segment() {
   signal=$(echo "$wifi" | grep "\sagrCtlRSSI" | sed -e "s/[^:]*: //" | sed -e "s/-//")
   ssid=$(echo "$wifi" |grep "\sSSID"|sed -e "s/[^:]*: //")
 
-  if [ $signal -le 39 ]; then
+  if [ ${signal} == 0 ]; then
+    echo "No NetWork"  
+  elif [ ${signal} - gt 0 ]&&[ ${signal} -le 39 ]; then
     echo "${WIFI_VERY_STRONG} ${ssid}"
-  elif [ $signal -gt 39 ]&&[ $signal -le 49 ]; then
+  elif [ ${signal} -gt 39 ]&&[ ${signal} -le 49 ]; then
     echo "${WIFI_STRONG} ${ssid}"
-  elif [ $signal -gt 49 ]&&[ $signal -le 59 ]; then
+  elif [ ${signal} -gt 49 ]&&[ ${signal} -le 59 ]; then
     echo "${WIFI_NORMAL} ${ssid}"
-  elif [ $signal -gt 59 ]&&[ $signal -le 69 ]; then
+  elif [ ${signal} -gt 59 ]&&[ ${signal} -le 69 ]; then
     echo "${WIFI_WEAK} ${ssid}"
   else 
-    echo "${WIFI_WEAK}"
+    echo "${WIFI_WEAK} ${ssid}"
   fi
 }
